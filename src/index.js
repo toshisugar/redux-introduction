ad73eab50187aec7c870f90b6d2764dead95e474
toshisugar <toshi.sugar.and.salt@gmail.com>
2019-08-04 15:05:01 +0900

    refactoring

--- src/index.js
+++ src/index.js
@@ -2,45 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import { createStore } from 'redux';
 
-const initialState = {
-  task: "",
-  tasks: []
-};
-
-//tasksReducerを定義
-function tasksReducer(state = initialState, action) {
-  switch (action.type) {
-    case "INPUT_TASK":
-      return {
-        ...state,
-        task: action.payload.task
-      };
-    case "ADD_TASK":
-      return {
-        ...state,
-        tasks: state.tasks.concat([action.payload.task])
-      };
-    default:
-      return state;
-  }
-}
-
-
 const store = createStore(tasksReducer);
-
-const inputTask = (task) => ({
-  type: "INPUT_TASK",
-  payload: {
-    task
-  }
-});
-
-const addTask = (task) => ({
-  type: "ADD_TASK",
-  payload: {
-    task
-  }
-});
 
 function TodoApp({ store }) {
   const { task, tasks } = store.getState();
