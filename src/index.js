3fd8223f0bd331046a24f626fe3a5d9a46c375a7
toshisugar <toshi.sugar.and.salt@gmail.com>
2019-08-04 12:59:06 +0900

    change reducer

--- src/index.js
+++ src/index.js
@@ -45,10 +45,15 @@ const inputTask = (task) => ({
 
 function tasksReducer(state = initialState, action) {
   switch (action.type) {
+    case "INPUT_TASK":
+      return {
+        ...state,
+        task: action.payload.task
+      };
     case "ADD_TASK":
       return {
         ...state,
-        tasks: state.tasks.concat([action.task])
+        tasks: state.tasks.concat([action.payload.task])
       };
     default:
       return state;
