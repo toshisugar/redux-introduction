e1042a897852c0dc89950b862924f14ef1cccc20
toshisugar <toshi.sugar.and.salt@gmail.com>
2019-08-06 00:13:29 +0900

    change index code to use Provider

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import { render } from 'react-dom';
 import tasksReducer from './reducers/tasks';
-import TodoApp from './components/TodoApp';
-import { createStore } from 'redux';
+import TodoApp from './containers/TodoApp';
 
 const store = createStore(tasksReducer);
+
+render(
+  <Provider store={store}>
+    <TodoApp />
+  </Provider>,
+  document.getElementById('root')
+);
 
 function renderApp(store) {
   render(
